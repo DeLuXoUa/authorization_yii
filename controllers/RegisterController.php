d44890c230fbe2de700068e87f9baf991faacd74
DeLuXoUa <spam@fix.if.ua>
2016-09-22 23:43:01 +0300

    Add manage page and register

---
+++ controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\User;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * RegisterController implements the CRUD actions for User model.
+ */
+class RegisterController extends Controller
+{
+
+    /**
+     * Lists all User models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $model = new User();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+}
